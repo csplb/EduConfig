a7b6b632d3d2e7f6f28b02c2ad8c0ec716fb7727
Marcin Badurowicz <m.badurowicz@pollub.pl>
2014-12-10 19:29:20 +0100

    automatic version numbering with gitversion

---
+++ EduConfig/Properties/AssemblyInfo.template.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+[assembly: AssemblyTitle("EduConfig")]
+[assembly: AssemblyDescription("EduConfig jest narzędziem do automatycznej konfiguracji sieci eduroam")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Politechnika Lubelska")]
+[assembly: AssemblyProduct("EduConfig")]
+[assembly: AssemblyCopyright("Copyright © Politechnika Lubelska 2013-2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b4213b86-90f6-4be3-839c-3e9a3e4b7a86")]
+
+[assembly: AssemblyVersion("${GfvSemVer}")]
+[assembly: AssemblyFileVersion("${GfvAssemblySemVer}")]
+[assembly: AssemblyInformationalVersion("${GfvFullSemVer}.${GfvBranchName}.${ShortSha}")]
